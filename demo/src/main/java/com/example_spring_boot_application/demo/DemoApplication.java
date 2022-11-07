20195e72da8748e32d9bcae2f35942663209f7ba
dillon.coleman <coleman399@gmail.com>
2022-11-07 00:01:43 -0600

    made more changes

--- demo/src/main/java/com/example_spring_boot_application/demo/DemoApplication.java
+++ demo/src/main/java/com/example_spring_boot_application/demo/DemoApplication.java
@@ -2,16 +2,20 @@ package com.example_spring_boot_application.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
+	@RestController
+	class HelloworldController {
+		
+		@GetMapping("/")
+		String hello() {
+			return "Hello Docker World !";
+		}
 	}
 
 	public static void main(String[] args) {
