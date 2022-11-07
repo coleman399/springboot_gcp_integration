dc6e164d7eb8f4d69bd3e48c1b5e01eff4bfc898
dillon.coleman <coleman399@gmail.com>
2022-11-06 20:49:24 -0600

    init commit

---
+++ demo/src/test/java/com/example_spring_boot_application/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example_spring_boot_application.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
